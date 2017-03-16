c5eed34b0f52179279d8a4896245a6247328494d
Stan Kondrat <stan.kondrat@gmail.com>
2017-03-17 00:05:49 +0300

    add on reject and catch

--- src/lib.rs
+++ src/lib.rs
@@ -18,24 +18,46 @@ pub struct Promise {
 
 impl Promise {
     pub fn new<F>(executor: F) -> Promise
-        where F: Send + 'static + Fn(&Fn()) {
+        where F: Send + 'static + Fn(&Fn(), &Fn()) {
         let handlers = Arc::new(Mutex::new(Some(Vec::new())));
-        let handlers_cloned = handlers.clone();
+        let handlers_cloned1 = handlers.clone();
+        let handlers_cloned2 = handlers.clone();
         thread::spawn(move || {
-            executor(& move || {
-                for handler in handlers_cloned.lock().unwrap().take().unwrap().into_iter() {
+            let resolve = move || {
+                for handler in handlers_cloned1.lock().unwrap().take().unwrap().into_iter() {
                     let handler: Handler = handler;
-                    (handler.handler)();
+                    if handler.resolve {
+                        (handler.handler)();
+                    }
                 }
-            });
+            };
+            let reject = move || {
+                for handler in handlers_cloned2.lock().unwrap().take().unwrap().into_iter() {
+                    let handler: Handler = handler;
+                    if !handler.resolve {
+                        (handler.handler)();
+                    }
+                }
+            };
+            executor(&resolve, &reject);
         });
 
         Promise { handlers: handlers }
     }
 
-    pub fn then<F>(&mut self, on_fulfilled: F) -> &mut Promise
+
+    pub fn then<F1, F2>(&mut self, on_fulfilled: F1, on_rejected: F2) -> &mut Promise
+        where F1: Send + 'static + Fn(), F2: Send + 'static + Fn() {
+        let handler1 = Handler{ resolve: true, handler: Box::new(on_fulfilled) };
+        let handler2 = Handler{ resolve: false, handler: Box::new(on_rejected) };
+        self.handlers.lock().unwrap().as_mut().unwrap().push(handler1);
+        self.handlers.lock().unwrap().as_mut().unwrap().push(handler2);
+        self
+    }
+
+    pub fn catch<F>(&mut self, on_rejected: F) -> &mut Promise
         where F: Send + 'static + Fn() {
-        let handler = Handler{ resolve: true, handler: Box::new(on_fulfilled) };
+        let handler = Handler{ resolve: false, handler: Box::new(on_rejected) };
         self.handlers.lock().unwrap().as_mut().unwrap().push(handler);
         self
     }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ use std::time::Duration;
 
 fn main() {
     println!("1");
-    let mut promise = Promise::new(|resolve| {
+    let mut promise = Promise::new(|resolve, reject| {
                                         thread::sleep(Duration::from_millis(100));
                                         println!("3");
                                         resolve();
-                                        println!("6");
+                                        // reject();
+                                        println!("5");
                                     });
-    promise.then(|| println!("4"));
-    promise.then(|| println!("5"));
-    
+    promise.then(|| println!("4 then resolve"), || println!("4 then reject"));
+    promise.catch(|| println!("catch"));
+
     println!("2");
 
     thread::sleep(Duration::from_millis(200));
